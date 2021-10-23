efee27cff06106effb17296adb06138989cca7f1
Maurice Beckman <maurice.beckman@gmail.com>
2021-10-23 22:21:04 +0200

    Code copy from non git project

---
+++ nppGeneratePasswords/Main.cs
@@ -0,0 +1,53 @@
+﻿using System.Windows.Forms;
+using Kbg.NppPluginNET.PluginInfrastructure;
+
+namespace Kbg.NppPluginNET
+{
+    class Main
+    {
+        internal const string PluginName = "nppGeneratePasswords";
+        static ConfigAndGenerate ConfigAndGenerate = null;
+
+        public static void OnNotification(ScNotification notification)
+        {  
+            // This method is invoked whenever something is happening in notepad++
+            // use eg. as
+            // if (notification.Header.Code == (uint)NppMsg.NPPN_xxx)
+            // { ... }
+            // or
+            //
+            // if (notification.Header.Code == (uint)SciMsg.SCNxxx)
+            // { ... }
+
+        }
+
+        internal static void CommandMenuInit()
+        {
+            PluginBase.SetCommand(0, "Config && Generate", myDockableDialog );
+        }
+
+        internal static void SetToolBarIcon()
+        {
+
+        }
+
+        internal static void PluginCleanUp()
+        {
+
+        }
+
+
+        internal static void myDockableDialog()
+        {
+            ConfigAndGenerate = new ConfigAndGenerate();
+
+            if (ConfigAndGenerate.ShowDialog() == DialogResult.OK)
+            {
+                MessageBox.Show("Passwords are generated!");
+            }
+
+            ConfigAndGenerate.Close();
+            ConfigAndGenerate = null;
+        }
+    }
+}
